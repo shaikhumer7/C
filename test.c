927d88adf44be1b6b1eea1373520cee4e939f30c
Shaikh Umer <lilumer7@gmail.com>
2025-07-17 10:16:38 +0530

    test

--- test.c
+++ test.c
@@ -1,17 +1,6 @@
-#include "header.h"
+#include <stdio.h>
 
 int main() {
-    int test = rand();
-    int test2= rand();
-    printf("this is the value of rand is %d\n",test);
-    printf("This is the value of second rand %d\n",test2);
-    printf("This is the value of RAND_MAX is %d",RAND_MAX);
-
-    int age = 19;
-    if (age > 18 )
-    printf("You are Elgible to Vote");
-    else 
-    printf("You are not eligible to vote"); 
+    printf("This is Shakee!");
     return 0;
-}   
-
+}
